1439e7d973f5bf0aaeae8f90e789f4758d7af033
SKairinos <stefan.kairinos@ocado.com>
2023-04-26 16:01:07 +0100

    placeholders for task tests

--- app/test_worksheet_1.py
+++ app/test_worksheet_1.py
@@ -1,2 +1,10 @@
 def test_task_1(next_turn, world_state, avatar_state):
-    assert next_turn(world_state, avatar_state) != "NORTH"
+    pass
+
+
+def test_task_2(next_turn, world_state, avatar_state):
+    pass
+
+
+def test_task_3(next_turn, world_state, avatar_state):
+    pass
---
+++ app/test_worksheet_2.py
@@ -0,0 +1,6 @@
+def test_task_1(next_turn, world_state, avatar_state):
+    pass
+
+
+def test_task_2(next_turn, world_state, avatar_state):
+    pass
