bee944090eb2a28962d97e23d625f93af16cf68c
SKairinos <stefan.kairinos@ocado.com>
2023-04-27 17:07:04 +0100

    add cors to flask

--- app/web.py
+++ app/web.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, Response
+from flask_cors import CORS
 from pydantic import ValidationError
 
 import service
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/", methods=["POST"])
